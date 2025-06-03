17907303ad7dbf0752df60963610961bc82dae29
Ash <ashb01012000@gmail.com>
2025-06-03 20:43:26 +0000

    forth commit

--- src/app/api/jobs/[id]/route.ts
+++
@@ -1,5 +0,0 @@
-// This route has been deprecated and its functionality moved to /api/jobs/[jobId]/route.ts
-// To avoid routing conflicts, this file is now an empty module.
-// It can be safely deleted.
-
-export {};
